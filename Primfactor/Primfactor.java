a6202ec394f61208348343f93141a69b05c6333c
jbrode2s <32834969+jbrode2s@users.noreply.github.com>
2021-05-18 16:21:35 +0200

    Update Primfactor.java

--- Primfactor/Primfactor.java
+++ Primfactor/Primfactor.java
@@ -4,46 +4,35 @@ import java.math.BigInteger;
 import java.util.*;
 
 public class Primfactor {
-	
-	// Memoization of factors.
-	static Map<BigInteger, List<BigInteger>> factors = new HashMap<>();
+	static Map<BigInteger, List<BigInteger>> faktor = new HashMap<>();
 	private static final BigInteger TWO = BigInteger.ONE.add(BigInteger.ONE);
 
-	public static List<BigInteger> factors(BigInteger n, boolean duplicates) {
-	  // Have we done this one before?
-	  List<BigInteger> f = factors.get(n);
-	  if (f == null) {
-	    // Start empty.
-	    f = new ArrayList<>();
-	    // Check for duplicates.
+	public static List<BigInteger> fac(BigInteger n, boolean duplicates) {
+	  List<BigInteger> BigIList = faktor.get(n);
+	  if (BigIList == null) {
+	    BigIList = new ArrayList<>();
 	    BigInteger last = BigInteger.ZERO;
-	    // Limit the range as far as possible.
 	    for (BigInteger i = TWO; i.compareTo(n.divide(i)) <= 0; i = i.add(BigInteger.ONE)) {
-	      // Can have multiple copies of the same factor.
 	      while (n.mod(i).equals(BigInteger.ZERO)) {
 	        if (duplicates || !i.equals(last)) {
-	          f.add(i);
+	          BigIList.add(i);
 	          last = i;
 	        }
-	        // Remove that factor.
 	        n = n.divide(i);
 	      }
 	    }
-	    if (n.compareTo(BigInteger.ONE) > 0) {
-	      // Could be a residue.
-	      if (duplicates || n != last) {
-	        f.add(n);
-	      }
+	    if (n.compareTo(BigInteger.ONE) > 0){
+	      if (duplicates || n != last) 
+	        BigIList.add(n);
 	    }
-	    // Memoize.
-	    factors.put(n, f);
+	    faktor.put(n, BigIList);
 	  }
-	  return f;
+	  return BigIList;
 	}
 
 	public static void main(String[] args) {
 		BigInteger bi = new BigInteger("600851475143");
-		for(BigInteger in : factors(bi, false)) {
+		for(BigInteger in : fac(bi, false)) {
 			System.out.println(in);
 		}
 	}
