e840b783e607abb061315ba0f0e9ea453f503745
jbrode2s <jbrodes@web.de>
2021-05-21 14:17:58 +0200

    Update

--- fibonacci/Fibonacci.java
+++ fibonacci/Fibonacci.java
@@ -3,10 +3,8 @@ package fibonacci;
 public class Fibonacci {
 	
 	public static int fibonacci(int n) {
-		if(n == 0) 
-			return 0;
-		else if (n == 1) 
-			return 1;
+		if(n <= 2) 
+			return (n > 0) ? 1 : 0;
 		else
 			return fibonacci(n - 1) + fibonacci(n - 2);
 	}
